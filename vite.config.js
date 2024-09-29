79778a5d7f6049a11dc8d8fae4aad6fc1805909c
Akito <ghostlx2016@gmail.com>
2024-09-29 16:46:31 +0800

    更新图标

--- vite.config.js
+++ vite.config.js
@@ -72,13 +72,8 @@ export default ({ mode }) =>
               type: "image/png",
             },
             {
-              src: "/images/icon/144.png",
-              sizes: "144x144",
-              type: "image/png",
-            },
-            {
-              src: "/images/icon/192.png",
-              sizes: "192x192",
+              src: "/images/icon/256.png",
+              sizes: "256x256",
               type: "image/png",
             },
             {
